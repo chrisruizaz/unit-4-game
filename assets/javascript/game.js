8edad2da733321647c78f9ff09c3b4b57c7c09bf
Christopher Ruiz <chrisruizaz@gmail.com>
2019-08-24 14:52:28 -0700

    made reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,28 +3,27 @@
 ////
 var newNumber = 0;
 var totalGems = 0;
-var gemBlue = randomIntFromInterval(50, 75);
+var gemPurple = randomIntFromInterval(50, 75);
 var gemGreen = randomIntFromInterval(35, 49);
 var gemOrange = randomIntFromInterval(20, 34);
 var gemRed = randomIntFromInterval(11, 19);
-var gemPurple = Math.floor(Math.random() * 10) + 1;
+var gemBlue = Math.floor(Math.random() * 10) + 1;
+var gemWhite = 1;
 
 var totalStones = [gemBlue, gemGreen, gemOrange, gemPurple, gemRed];
 // totalStones.length
 // newNumber = random number from 1-totalStones.length
 //+ random number, thats NOT the same number, from 1-totalSTones
-// add those two numbers together.
 // for loop is needed.
 for (i = 0; i < totalStones.length; i++) {
-  newNumber += totalStones[randomIntFromInterval(0, totalStones.length - 1)]; //I know what happened. you were looking for index 0-6 it only has 0-5
+  newNumber += totalStones[randomIntFromInterval(0, totalStones.length - 1)];
 }
 console.log(`newNumber equals ${newNumber}`);
-// newNumber = totalstones[2] + totalstones[0]
+// wins & loses
 var wins = 0;
 var loses = 0;
-var totalScore;
-var firstGemClicked;
-var secondGemClicked;
+//the user's total
+var totalScore = 0;
 ////
 //global functions
 ////
@@ -39,20 +38,16 @@ $(document).ready(function() {
   console.log("ready!");
   document.getElementById("newNumber").innerHTML = newNumber;
 });
+
 function gemSelected(x) {
   GemClicked = x;
-  // if firstGemClicked has a value then add to secondGemClicked. Then add first and second to see if they are the same.
-
-  // if the guess is correct then wins++
-  // if the guess was wrong then set the value of firstGEmClicked and secondGemClicked to nothing
+  totalScore += x;
+  document.getElementById("userScore").innerHTML = totalScore;
   console.log(x);
+  console.log(`User Gem Total is: ${totalScore}`);
+  isthegameover(newNumber, totalScore);
 }
-if (newNumber === totalScore) {
-  wins++, (document.getElementById("wins").innerHTML = wins);
-}
-else (newNumber<totalScore){
-    loses++, (document.getElementById("loses").innerHTML = loses);
-}
+
 ////
 //crystal function
 ////
@@ -72,5 +67,48 @@ $("#red").click(function() {
 $("#purple").click(function() {
   gemSelected(gemPurple);
 });
-
+$("#white").click(function() {
+  gemSelected(gemWhite);
+});
 $(".card-text").append("<p></p>");
+
+function isthegameover(targetScore, userScore) {
+  if (userScore > targetScore) {
+    //user lost
+    alert("You Lost");
+    loses++;
+    resetlose();
+    document.getElementById("loses").innerHTML = loses;
+  } else if (userScore == targetScore) {
+    //user wins
+    alert("You Won");
+    wins++;
+    document.getElementById("wins").innerHTML = wins;
+    resetwin();
+  } else {
+    //nothing happens
+    console.log("keep clicking gems");
+    return;
+    alert(`I said stop@!!!!!!`);
+  }
+}
+function resetlose() {
+  //change total score to 0 so you dont lose after winning
+  totalScore = 0;
+  for (i = 0; i < totalStones.length; i++) {
+    //gets easier with lose
+    newNumber += totalStones[randomIntFromInterval(0, totalStones.length - 1)];
+  }
+  document.getElementById("userScore").innerHTML = totalScore;
+  document.getElementById("newNumber").innerHTML = newNumber;
+}
+function resetwin() {
+  //change total score to 0 so you dont lose after winning
+  totalScore = 0;
+  for (i = 0; i < totalStones.length; i++) {
+    //gets harder with wins
+    newNumber = totalStones[randomIntFromInterval(0, totalStones.length - 1)];
+  }
+  document.getElementById("userScore").innerHTML = totalScore;
+  document.getElementById("newNumber").innerHTML = newNumber;
+}
